3a63fc4280455b4ba8a2bfd3f8901a78e926fd55
Farbod Salamat-Zadeh <12140044+farbodsz@users.noreply.github.com>
2021-07-14 12:18:29 +0100

    Improve telescope find/grep command args

--- neovim/.config/nvim/lua/farbodsz/telescope.lua
+++ neovim/.config/nvim/lua/farbodsz/telescope.lua
@@ -4,6 +4,7 @@
 local M = {}
 
 local actions = require("telescope.actions")
+local builtin = require("telescope.builtin")
 
 require("telescope").setup {
   defaults = {
@@ -20,25 +21,6 @@ require("telescope").setup {
         ["<c-j>"] = actions.move_selection_next,
       }
     },
-
-    pickers = {
-      find_files = {
-        find_command = {
-          "rg",
-          "--files",
-          "--hidden",
-        },
-      }
-    },
-
-    vimgrep_arguments = {
-      "rg",
-      "--with-filename",
-      "--hidden",
-      "--line-number",
-      "--column",
-      "--smart-case",
-    }
   },
 
   extensions = {
@@ -52,19 +34,46 @@ require("telescope").setup {
 require("telescope").load_extension("fzy_native")
 
 
+local opts_find_command = {
+  "rg",
+  "--files",
+  "--hidden",
+  "-g",
+  "!{.git,.cache,node_modules}",
+}
+
+local opts_vimgrep_args = {
+  "rg",
+  "--column",
+  "--no-heading",
+  "--line-number",
+  "--color=always",
+  "--hidden",
+  "-g",
+  "!{.git,.cache,node_modules}",
+}
+
+M.find_files = function()
+  builtin.find_files({find_command = opts_find_command})
+end
+
+M.live_grep = function()
+  builtin.live_grep({vimgrep_arguments = opts_vimgrep_args})
+end
+
 M.search_dotfiles = function()
-  require("telescope.builtin").find_files({
+  builtin.find_files({
+    find_command = opts_find_command,
     prompt_title = "Dotfiles",
     cwd = "~/.dotfiles",
-    hidden = true,
   })
 end
 
 M.search_home = function()
-  require("telescope.builtin").find_files({
+  builtin.find_files({
+    find_command = opts_find_command,
     prompt_title = "Home",
     cwd = "~/",
-    hidden = true,
   })
 end
 
