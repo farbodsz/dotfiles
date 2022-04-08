99d4856f7fd64d96d25e09a509c36e62a7c59eef
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2022-04-08 15:22:14 +0100

    fix(snippets): mappings for jumping to snippet positions

--- neovim/.config/nvim/lua/farbodsz/completion.lua
+++ neovim/.config/nvim/lua/farbodsz/completion.lua
@@ -3,6 +3,7 @@
 -------------------------------------------------------------------------------
 
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 local lspkind = require("lspkind")
 
 local has_setup = false
@@ -18,6 +19,15 @@ local function setup_cmp_dictionary()
   })
 end
 
+local function has_words_before()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0
+    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
+        :sub(col, col)
+        :match("%s")
+      == nil
+end
+
 function M.setup()
   if has_setup then
     return
@@ -27,9 +37,36 @@ function M.setup()
     mapping = {
       ["<C-d>"] = cmp.mapping.scroll_docs(-4),
       ["<C-u>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.close(),
+      ["<C-Space>"] = cmp.mapping.complete(),
       ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+      ["<C-j>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
+      ["<C-k>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, {
+        "i",
+        "s",
+      }),
     },
 
     sources = {
@@ -61,7 +98,7 @@ function M.setup()
 
     snippet = {
       expand = function(args)
-        require("luasnip").lsp_expand(args.body)
+        luasnip.lsp_expand(args.body)
       end,
     },
   })
