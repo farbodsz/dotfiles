5a3ef111e749b5e45429bb4a15ef56831c7e341f
Farbod Salamat-Zadeh <farbodsalamat@gmail.com>
2022-04-14 15:07:47 +0100

    fix(maps): cmp mapping `<C-k>` interferes with digraphs

--- neovim/.config/nvim/lua/farbodsz/completion.lua
+++ neovim/.config/nvim/lua/farbodsz/completion.lua
@@ -19,13 +19,20 @@ local function setup_cmp_dictionary()
   })
 end
 
-local function has_words_before()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0
-    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
-        :sub(col, col)
-        :match("%s")
-      == nil
+local function jump_prev_snippet(fallback)
+  if luasnip.jumpable(-1) then
+    luasnip.jump(-1)
+  else
+    fallback()
+  end
+end
+
+local function jump_next_snippet(fallback)
+  if luasnip.expand_or_jumpable() then
+    luasnip.expand_or_jump()
+  else
+    fallback()
+  end
 end
 
 function M.setup()
@@ -35,38 +42,14 @@ function M.setup()
 
   cmp.setup({
     mapping = {
-      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-u>"] = cmp.mapping.scroll_docs(4),
+      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-t>"] = cmp.mapping.scroll_docs(4),
       ["<C-e>"] = cmp.mapping.close(),
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<CR>"] = cmp.mapping.confirm({ select = true }),
 
-      ["<C-j>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
-      ["<C-k>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, {
-        "i",
-        "s",
-      }),
+      ["<C-i>"] = cmp.mapping(jump_next_snippet, { "i", "s" }),
+      ["<C-u>"] = cmp.mapping(jump_prev_snippet, { "i", "s" }),
     },
 
     sources = {
